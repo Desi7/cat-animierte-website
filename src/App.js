dc6f660c4045c518843646494e727efc34e296ab
Desi7 <desi.02@outlok.com>
2023-03-13 16:44:02 +0100

    testing :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Container} from "react-bootstrap";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 className="display-1 p-3" style={{backgroundColor: "orange"}}>Hellooo</h1>
+    </Container>
   );
 }
-
-export default App;
