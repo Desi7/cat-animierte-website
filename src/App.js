16e987218dc914806ae50a2d503af58a307e91c6
Desi7 <desi.02@outlok.com>
2023-04-03 16:30:18 +0200

    sub-title finished (?sparkly)

--- src/App.js
+++ src/App.js
@@ -15,7 +15,15 @@ export default function App() {
     return (
         <div className="container vh-100 d-flex align-items-center">
             <h1 className="text-center">CATIFICATION</h1>
-            <h4>Test</h4>
+            <h4 className="text-center">
+                Welcome to our website dedicated to all things feline! We are a one-stop-shop for cat lovers,
+                offering a wide range of high-quality cat products to help you pamper your furry friends. Whether
+                you're a new cat owner or a seasoned pro, we've got you covered with everything from food and treats
+                to toys and accessories. Our team of dedicated cat enthusiasts carefully curate our selection of
+                products to ensure that only the very best items make it onto our virtual shelves. We believe that
+                every cat deserves the very best, which is why we only stock products that are safe, healthy, and
+                beneficial for your furry companions. So why not take a look around and see what we have to offer?
+                <br/>We're sure you'll find something that your feline friend will love!</h4>
 
             <CardFacingRight title="Flippity Fish" text="text..." image={flippityFish} colorOfButton='#2b26c3'/>
             <CardFacingLeft title="Cat Pillow" image={catPillow} text="text..." colorOfButton='#2b26c3'/>
@@ -36,6 +44,8 @@ export default function App() {
                     </Col>
                 </Row>
             </Bounce>
+
+            
 
             <Footer/>
         </div>
