269f18bdfdb697c0b4b8761997c6dd6e3e78df7e
Desi7 <desi.02@outlok.com>
2023-04-03 10:47:34 +0200

    onscroll animation not finished + pushing status

--- src/App.js
+++ src/App.js
@@ -1,93 +1,121 @@
 import './App.css';
 import {Button, Card, Col, Row} from "react-bootstrap";
+import {Fade, Zoom} from "react-reveal";
 import KittyIcon from "./KittyIcon";
 import flippityFish from './images/flippity-fish.jpg';
 import katzenKissen from './images/katzenkissen.jpg';
 import katzenburg from './images/katzenburg.jpg';
 import katzenBadewanne from './images/katzen-badewanne.jpg';
+import katzenToilette from './images/katzen-toilette.jpg';
+import {Parallax, ParallaxLayer} from '@react-spring/parallax'
 
 export default function App() {
+
     return (
         <div className="container vh-100 d-flex align-items-center">
-            {/*Heftiger Header mit viel bling bling*/}
             <h1 className="text-center">CATIFICATION</h1>
 
-            {/*Für unsere Produkte - immer mit Text(innen) und Bild(aussen)*/}
-            <div data-aos="zoom-out-left">
-                <Card>
-                    <p className="text-right">text....</p>
-                    <Row>
-                        <Col>
-                            <img className="pic" src={flippityFish} alt="Flippity Fish"></img>
-                        </Col>
-                        <Col style={{alignSelf: "flex-end"}}>
-                            <span>hover here</span>
-                            <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
-                        </Col>
-                    </Row>
-                    <h3>Flippity Fish</h3>
-                </Card>
-            </div>
-
-            <div data-aos="zoom-out-left">
-                <Card>
-                    <Row>
-                        <Col style={{alignSelf: "flex-end"}}>
-                            <span>hover here</span>
-                            <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
-                        </Col>
-                        <Col>
-                            <img alt="katzen-kissen" src={katzenKissen} className="pic"></img>
-                        </Col>
-                    </Row>
-                    <h3>Katzenkissen</h3>
-                    <p className="text-left">text....</p>
-                </Card>
+            <div>
+                <Fade left>
+                    <Card>
+                        <p className="text-right">text....</p>
+                        <Row>
+                            <Col>
+                                <img className="pic" src={flippityFish} alt="Flippity Fish"></img>
+                            </Col>
+                            <Col style={{alignSelf: "flex-end"}}>
+                                <span>hover here</span>
+                                <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
+                            </Col>
+                        </Row>
+                        <h3>Flippity Fish</h3>
+                    </Card>
+                </Fade>
             </div>
 
             <div>
-                <Card>
-                    <p className="text-right">text....</p>
-                    <Row>
-                        <Col>
-                            <img className="pic" src={katzenburg} alt="Katzenburg"></img>
-                        </Col>
-                        <Col style={{alignSelf: "flex-end"}}>
-                            <span>hover here</span>
-                            <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
-                        </Col>
-                    </Row>
-                    <h3>Katzenburg</h3>
-                </Card>
+                <Fade right>
+                    <Card>
+                        <Row>
+                            <Col style={{alignSelf: "flex-end"}}>
+                                <span>hover here</span>
+                                <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
+                            </Col>
+                            <Col>
+                                <img alt="katzen-kissen" src={katzenKissen} className="pic"></img>
+                            </Col>
+                        </Row>
+                        <h3>Katzenkissen</h3>
+                        <p className="text-left">text....</p>
+                    </Card>
+                </Fade>
             </div>
 
             <div>
-                <Card>
-                    <p className="text-left">text....</p>
-                    <Row>
-                        <Col style={{alignSelf: "flex-end"}}>
-                            <span>hover here</span>
-                            <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
-                        </Col>
-                        <Col>
-                            <img className="pic" src={katzenBadewanne} alt="Katzenburg"></img>
-                        </Col>
-                    </Row>
-                    <h3>Badewanne</h3>
-                </Card>
+                <Fade left>
+                    <Card>
+                        <p className="text-right">text....</p>
+                        <Row>
+                            <Col>
+                                <img className="pic" src={katzenburg} alt="Katzenburg"></img>
+                            </Col>
+                            <Col style={{alignSelf: "flex-end"}}>
+                                <span>hover here</span>
+                                <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
+                            </Col>
+                        </Row>
+                        <h3>Katzenburg</h3>
+                    </Card>
+                </Fade>
             </div>
-            <Row>
-                <Col>
-                    <KittyIcon/>
-                </Col>
-                <Col>
-                    <KittyIcon/>
-                </Col>
-                <Col>
-                    <KittyIcon/>
-                </Col>
-            </Row>
 
+            <div>
+                <Fade right>
+                    <Card>
+                        <p className="text-left">text....</p>
+                        <Row>
+                            <Col style={{alignSelf: "flex-end"}}>
+                                <span>hover here</span>
+                                <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
+                            </Col>
+                            <Col>
+                                <img className="pic" src={katzenBadewanne} alt="Katzenburg"></img>
+                            </Col>
+                        </Row>
+                        <h3>Badewanne</h3>
+                    </Card>
+                </Fade>
+            </div>
+            <div>
+                <Fade left>
+                    <Card>
+                        <p className="text-right">text....</p>
+                        <Row>
+                            <Col>
+                                <img className="pic" src={katzenToilette} alt="Katzentoilette"></img>
+                            </Col>
+                            <Col style={{alignSelf: "flex-end"}}>
+                                <span>hover here</span>
+                                <Button disabled={true} style={{backgroundColor: '#2b26c3'}}></Button>
+                            </Col>
+                        </Row>
+                        <h3>Katzentoilette</h3>
+                    </Card>
+                </Fade>
+            </div>
+            <Zoom top>
+                <Row>
+                    <Col>
+                        <KittyIcon/>
+                    </Col>
+                    <Col>
+                        <KittyIcon/>
+                    </Col>
+                    <Col>
+                        <KittyIcon/>
+                    </Col>
+                </Row>
+            </Zoom>
         </div>
     );
 }
