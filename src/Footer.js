d392610f8bc71b31741507a17739380a4be76d27
Desi7 <desi.02@outlok.com>
2023-04-03 15:29:12 +0200

    sub-title in progress + Footer in progress

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,8 @@
-import {MDBFooter, MDBContainer, MDBIcon, MDBBtn} from 'mdb-react-ui-kit';
+import {MDBFooter} from 'mdb-react-ui-kit';
 export default function Footer() {
 
     return(
-        <div style={{width: "1500px"}}>
+        <div style={{width: "100%"}}>
             <MDBFooter className='bg-light text-center text-white'>
                 <div className='text-center p-3' style={{ backgroundColor: "lightgray" }}>
                     <p className='text-black'>© 2023 Copyright:<br/> Desi7/cat-animierte-website</p>
